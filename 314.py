e053b128b8ddbff7d525a62aee862551864ff619
Michael <106803772+anon4K@users.noreply.github.com>
2024-02-23 09:10:35 +0100

    Add files via upload

---
+++ 314.py
@@ -0,0 +1,21 @@
+print("Welcome to my channel.")
+my_age = int(input("How old are you? "))
+my_class = str(input("What class are you in? "))
+my_schoolName = input("What is the name of your school? ")
+
+
+if my_age >= 14:
+    print("You are within the age bracket.")
+else:
+    print("Please leave, you don't meet the age requirement for viewing this channel.")
+
+if my_class == "jss3":
+    print("You are welcome!")
+else:
+    print("Please leave!")
+
+if my_schoolName == "CJPAC":
+    print("You are welcome to CJPAC.")
+else:
+    print("You are in CJPAC.")
+
